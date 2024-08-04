1d8f89502862808b610bdff9eb1ca8ee12dad9aa
sina karamiyan <sinakn78@gmail.com>
2024-08-05 00:10:50 +0330

    js file for admin dashboard pag

---
+++ core/educationalapp/static/js/dashboard.js
@@ -0,0 +1,24 @@
+class preloader{
+    constructor(){
+        this.preloader();
+    }
+    preloader(){
+        const p = document.querySelector('#preloader');
+        window.addEventListener('DOMContentLoaded', () => setTimeout(() => {
+            p.classList.add('hidden');
+        }, 500));
+
+        //admin menu toggler to hide and show with translate-x
+        const menu = document.querySelector('#menu');
+        const menutoggle = document.querySelector('#menu-toggle');
+        const aside = document.querySelector('#aside');
+        menu.addEventListener('click', (e)=>{
+            aside.classList.remove('translate-x-full')
+        })
+        menutoggle.addEventListener('click', (e)=> {
+            aside.classList.add('translate-x-full');
+        })
+    }
+}
+
+new preloader();
