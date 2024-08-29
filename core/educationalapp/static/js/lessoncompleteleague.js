0d72fda5a55d04513a2cfd468823dfa754c3b4de
sina karamiyan <sinakn78@gmail.com>
2024-08-29 13:36:41 +0330

    edit course and scroll animation for lessoncompleteleague page

--- core/educationalapp/static/js/lessoncompleteleague.js
+++ core/educationalapp/static/js/lessoncompleteleague.js
@@ -18,7 +18,7 @@ class lessoncompleteleague{
         // changable element
         let change = 7
         // change score number
-        score[change].innerHTML = parseInt(980);
+        score[change].innerHTML = parseInt(900);
 
         // get related changable children in sort div
         const sort2 = sort.children[change];
@@ -27,18 +27,21 @@ class lessoncompleteleague{
         sort2.style.background = 'rgba(255, 255, 255, 1)'; // glassy background color
         sort2.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)'; // subtle shadow
         sort2.style.backdropFilter = 'blur(2px)'; // glassy blur effect
-        sort2.styleborderRadius = '10px'; // rounded corners
+        sort2.style.borderRadius = '10px'; // rounded corners
         sort2.style.marginLeft = '10px'; // marginleft
         sort2.style.marginRight = '10px'; //marginright
         sort2.style.transition = 'transform 4s'; // add trnasition effect to element.
+        
+        // first scroll to target changable element
+        sort2.scrollIntoView();
         
         for (let i = change; i > 0 ; i--) {
             
             if (parseInt(score[change].innerHTML) >= parseInt(score[i-1].innerHTML)){
                 // chabge place of two pervious element
                 const sort1 = sort.children[i-1];   
-                const sortp = sort.children[i]; 
-
+                const sortp = sort.children[i];           
+                
                 // change rank number of user
                 const rankmain = parseInt(sort2.firstElementChild.querySelector('#rank').innerHTML);
                 sort2.firstElementChild.querySelector('#rank').innerHTML = rankmain - 1;
@@ -54,6 +57,10 @@ class lessoncompleteleague{
                 sort1.style.transform = 'translateY(' + (sortp.offsetTop - sort1.offsetTop) + 'px)';
                 sort2.style.transform = 'translateY(' + (sort1.offsetTop - sort2.offsetTop) + 'px)';
 
+                // at each loop scroll to pervious element for better animation
+                setTimeout(() => {
+                    sortp.scrollIntoView({ behavior: "smooth", block: "end" });
+                }, 1000);
             }
         }
 
