7069393fd1e685e479ae425a4562454ec3b21763
sina karamiyan <sinakn78@gmail.com>
2024-10-04 16:10:48 +0330

    "Removed imports for settings and static, added debug toolbar URL"

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # add debug toolbar to apps
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('educationalapp.urls')),
 ] 
