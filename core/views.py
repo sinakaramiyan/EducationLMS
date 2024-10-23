4d8617543cba3eab641c85b4015beb59570c8824
sina karamiyan <sinakn78@gmail.com>
2024-10-23 12:21:18 +0330

    " Add alpine js configuration and installation and delete some extra code  "

--- core/views.py
+++ core/views.py
@@ -1,10 +1,2 @@
-from django.urls import reverse_lazy
-from django.views import generic
-from core.forms import UserRegistrationForm
 
 # Create your views here.
-
-class SignUpView(generic.CreateView):
-    form_class = UserRegistrationForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')
