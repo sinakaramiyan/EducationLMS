95a9ee67cde704f2d53679060bba5d54df63c207
sina karamiyan <sinakn78@gmail.com>
2024-10-23 21:55:24 +0330

    " Add created models in django admin "

--- individual_course_management/admin.py
+++ individual_course_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# Get all models from your app
+models = apps.get_app_config('individual_course_management').get_models()
+
+# Loop through the models and register them
+for model in models:
+    admin.site.register(model)
