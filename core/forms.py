8f71999efb9bb44916949bdc4e26d838c5acd2fd
sina karamiyan <sinakn78@gmail.com>
2024-10-24 00:28:55 +0330

    " Update signup form fields "

--- core/forms.py
+++ core/forms.py
@@ -3,10 +3,46 @@ from allauth.account.forms import SignupForm
 from django import forms
 
 class UserRegistrationForm(SignupForm):
+    first_name = forms.CharField(
+        label='نام',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input input-bordered text-md',
+                'placeholder': 'نام'
+            }),
+    )
+
+    last_name = forms.CharField(
+        label='نام خانوادگی',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input input-bordered text-md',
+                'placeholder': 'نام خانوادگی'
+            }),
+    )
+
+    phone = forms.CharField(
+        label='موبایل',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input input-bordered text-md',
+                'placeholder': 'موبایل'
+            }),
+    )
+
+    email = forms.CharField(
+        label='ایمیل',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'input input-bordered text-md',
+                'placeholder': 'ایمیل'
+            }),
+    )
+
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'phone', 'email', 'password1', 'password2')
-
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
@@ -24,10 +60,14 @@ class UserRegistrationForm(SignupForm):
         })
         
         # Add autofocus to the first_name field
-        # self.fields['first_name'].widget.attrs.update({
-        #     'autofocus': 'autofocus'
-        # })
+        self.fields['first_name'].widget.attrs.update({
+            'autofocus': 'autofocus'
+        })
     
     def save(self, request):
         user = super(UserRegistrationForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.phone = self.cleaned_data['phone']
+        user.save()
         return user
